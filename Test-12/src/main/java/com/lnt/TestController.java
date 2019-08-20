93fad5da21ee63db27947ba24f303970ebcafdf3
AE205_PC1 <AE205_PC1@Infva06510>
2019-08-20 13:00:29 -0700

    new

---
+++ Test-12/src/main/java/com/lnt/TestController.java
@@ -0,0 +1,14 @@
+package com.lnt;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class TestController {
+	@GetMapping("/")
+	public String hello()
+	{
+		return "Hello";
+	}
+}
