9dfd2b3e960059e9656e7f85fba0e48805d0e9c1
AE205_PC1 <AE205_PC1@Infva06510>
2019-08-20 13:00:39 -0700

    new

--- Test-12/src/main/java/com/lnt/Test12Application.java
+++ Test-12/src/main/java/com/lnt/Test12Application.java
@@ -7,11 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @SpringBootApplication
 public class Test12Application {
 
-	@GetMapping("/")
-	public String hello()
-	{
-		return "Hello";
-	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Test12Application.class, args);
 	}
